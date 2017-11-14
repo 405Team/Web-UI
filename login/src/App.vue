af38fe36afdf76fad52c82ab531443ffe1235553
wk <656046127@qq.com>
2017-11-14 17:08:44 +0800

    添加main2

--- login/src/App.vue
+++ login/src/App.vue
@@ -13,6 +13,32 @@
         </div>
       </div>
     </div>
+    <div class="main2" >
+      <div class="wrapper">
+
+        <!-- <div id="banner"> -->
+          <div id="news">
+            <div class="news-icon" title="News">
+              <div id="hot-news">
+                <!-- <marquee id="affiche" align="left" behavior="scroll" bgcolor="#FF0000" direction="up" height="300" width="200" hspace="50"
+                vspace="20" loop="-1" scrollamount="10" scrolldelay="100" onMouseOut="this.start()" onMouseOver="this.stop()"> -->
+                <marquee hspace="0" vspace="0" align="left" behavior="scroll" height="40" width="930" bgcolor="red" direction="left"
+                loop="-1" scrollamount="10" scrolldelay="100" onMouseOut="this.start()" onMouseOver="this.stop()">
+                {{ $WebInfo.news.prompt }}
+                </marquee>
+                <!-- <a href="javascript: void(0)" ng-click="newsClick()">{{ $WebInfo.news.prompt }}</a> -->
+              </div>
+            </div>
+          </div>
+        <!-- </div> -->
+
+        <el-carousel class="carousel" :interval="3000" type="card" height="300px" indicator-position='none' arrow='never'>
+          <el-carousel-item v-for="item in 3" :key="item">
+            <h3>{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </div>
     <div class="footer">
       <div class="wrapper">
         <footerView></footerView>
@@ -70,7 +96,8 @@ export default {
 <style lang="scss">
 $wrapperWidth: 1000px;
 $headerHeight: 120px;
-$mainHeight: 800px;
+$mainHeight: 442px;
+$main2Height: 400px;
 $footerHeight: 350px;
 
 #app {
@@ -92,14 +119,64 @@ $footerHeight: 350px;
   width:100%;
   height: $mainHeight;
   background:url('./assets/main_bg.png') repeat-x center top;
+  .login-content {
+    padding: 30px 0 0 5px;
+    width: 235px;
+    height: 242px;
+    // border: 1px solid #2f1c20;
+    background:url('./assets/login_bg.png') no-repeat center;
+    float: right;
+  }
 }
-.login-content {
-  padding: 30px 0 0 5px;
-  width: 235px;
-  height: 242px;
-  // border: 1px solid #2f1c20;
-  background:url('./assets/login_bg.png') no-repeat center;
-  float: right;
+
+.main2 {
+  padding: 0;
+  z-index:10;
+  width:100%;
+  height: $main2Height;
+  background:url('./assets/main2_bg.jpg') repeat-x center top;
+
+  #news{
+    position:relative;width:100%;height:40px
+  }
+  #news .news-icon:before{
+    content:attr(title);background:url('./assets/new_img.png') no-repeat left center;
+    padding-left:20px;
+    color:#ffd053;
+    font-size:14px;text-align:left;line-height:40px
+  }
+  #hot-news{
+    float:right;height:40px;width:930px;white-space:nowrap;overflow:hidden
+  }
+  #hot-news a{
+    color:#fff;font-size:14px;text-decoration:none;line-height:40px
+  }
+  #hot-news a:hover{
+    text-decoration:underline
+  }
+  .marqueen{
+    height: 40px;
+    line-height: 40px;
+  }
+  .carousel {
+    padding-top: 50px;
+    width: 80%;
+  }
+  .el-carousel__item h3 {
+      color: #475669;
+      font-size: 14px;
+      opacity: 0.5;
+      line-height: 200px;
+      margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 }
 .footer {
   padding: 0;
